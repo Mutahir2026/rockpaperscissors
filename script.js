9058143a572389d8ea7c4349558abe7de4f536fe
Mutahir Mustafa <s.mutahirmustafa@gmail.com>
2026-08-28 16:22:06 +0500

    add more divs

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let paper=document.getElementById("paper");
 let scissors=document.getElementById("scissors");
 let rps=["rock","paper","scissors"];
 let playerwincount=0,compwincount=0;
-function checkPlayerWin()
+let urscore=document.getElementById("urscore");
+let compscore=document.getElementById("compscore");
+function checkPlayerWin(randomnum)
 {
     if (rock.id=="rock"&&rps[randomnum]=="scissors") return true;
     else if (rock.id=="paper"&&rps[randomnum]=="rock") return true;
@@ -11,20 +13,26 @@ function checkPlayerWin()
     else if (rock.id==rps[randomnum]) return null;
     else  return false;  
 }
-rock.addEventListener("click",function()
+let Func=function()
 {
 let randomnum=Math.floor(Math.random()*3);
-if (checkPlayerWin())
+let res=checkPlayerWin(randomnum)
+if (res)
 {
      playerwincount++;
+     urscore.innerHTML="You : "+ playerwincount;
 }
-else if (checkPlayerWin()==null)
+else if (res==null)
 {
-   continue;
+console.log("Draw");
 }
 else 
 {
     compwincount++;
+     compscore.innerHTML="Computer : "+compwincount;
+
 }
 }
-);
+rock.addEventListener("click",Func);
+paper.addEventListener("click",Func);
+scissors.addEventListener("click",Func);
